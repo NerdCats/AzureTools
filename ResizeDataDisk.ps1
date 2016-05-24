0ba8d0700ad06edf6949094097074ae79084e2fd
Swagata Prateek <s.prateek@hotmail.com>
2016-05-24 12:11:43 +0600

    Disk resize script indentation fixes

--- ResizeDataDisk.ps1
+++ ResizeDataDisk.ps1
@@ -4,70 +4,39 @@ Add-AzureAccount
 
 # Select Azure Subscription
  
-$subscription = 
-    (Get-AzureSubscription).SubscriptionName `
-    | Out-GridView ` 
-        -Title "Select Azure Subscription" `
-         -PassThru
+$subscription = (Get-AzureSubscription).SubscriptionName ` | Out-GridView ` -Title "Select Azure Subscription" ` -PassThru
  
-Select-AzureSubscription `
-    -SubscriptionName $subscription `
-    -Current
+Select-AzureSubscription ` -SubscriptionName $subscription ` -Current
 
 # Select Azure Storage Account
-$storageAccount = 
-    (Get-AzureStorageAccount).Label `
-    | Out-GridView `
-        -Title "Select Azure Storage Account" `
-        -PassThru
+$storageAccount = (Get-AzureStorageAccount).Label ` | Out-GridView ` -Title "Select Azure Storage Account" ` -PassThru
  
-Set-AzureSubscription `
-    -SubscriptionName $subscription `
-    -CurrentStorageAccountName $storageAccount
+Set-AzureSubscription ` -SubscriptionName $subscription ` -CurrentStorageAccountName $storageAccount
 
 # Select Azure VM
  
-$vm = 
-    Get-AzureVM `
-    | Out-GridView `
-        -Title "Select a VM …" `
-         -PassThru
+$vm = Get-AzureVM ` | Out-GridView ` -Title "Select a VM …" ` -PassThru
 
 # Select Data Disk to resize
  
-$disk = 
-    $vm `
-    | Get-AzureDataDisk `
-    | Out-GridView `
-        -Title "Select a data disk to resize" `
-        -PassThru
+$disk = $vm ` | Get-AzureDataDisk ` | Out-GridView ` -Title "Select a data disk to resize" ` -PassThru
  
 $diskName = $disk.DiskName
 
 # Specify new Data Disk size – must be larger than current size
  
 do {
- 
-    $size = 
-        Read-Host `
-            -Prompt "New size in GB"
-
+    $size = Read-Host ` -Prompt "New size in GB"
 }
-
 until ( $size -gt $disk.LogicalDiskSizeInGB )
 
 # Stop and Deallocate VM prior to resizing data disk
  
-$vm `
-    | Stop-AzureVM `
-         -Force
+$vm ` | Stop-AzureVM ` -Force
 
 # Resize Data Disk to Larger Size
  
-Update-AzureDisk `
-    -Label "$diskName" `
-    -DiskName "$diskName" `
-    -ResizedSizeInGB $size
+Update-AzureDisk ` -Label "$diskName" ` -DiskName "$diskName" ` -ResizedSizeInGB $size
 
 # Start VM
 
